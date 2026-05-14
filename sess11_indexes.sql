7b71c7b97e3be9d7c3f9f30c3c9c5b498f1fd961
mapena111 <olomomoses75@gmail.com>
2026-05-14 12:42:51 +0300

    Add files via upload

--- sess11_indexes.sql
+++ sess11_indexes.sql
@@ -1,126 +1,130 @@
-/* this seesion cover how to work with indexes/ indeces in SQL server.*/
+/* This session cover how to work with indexes/indeces in SQL server. */
 
---Switch to the customer database 
-use Cust_db_adse2509
+-- Switch to the customer database
+use Cust_db_adse2509;
 
--- Demonstrate creating, modifying and deleting indexes 
-
---1. Create an Employee_details table 
+-- ---------------------------------------------------------------------------
+-- Demonstrate creating, modifying and deleting indexes
+-- ---------------------------------------------------------------------------
+-- 1. Create an Employee_details table
 if OBJECT_ID('Cust_Details') is null
 	Create table Cust_Details
 	(
-		EmpID int not null primary key,
-		AccNo nvarchar(18) not null
+		EmpID int not null identity(1001,1) primary key,
+		AccNo nvarchar(10) not null,
 		AccName nvarchar(180) not null,
 		Country nvarchar(70) not null
 	);
 else
-	print('the ''Cust_Details'' table already exists and will not be recreated')
+	print('The ''Cust_Details'' table already exists and will not be recreated')
 
---2 insert rows into Cust_Details
+-- 2. Insert/add rows into the Cust_details table
 Insert into dbo.Cust_Details
-(AccNo, AccName, Country)
+(AccNo,AccName,Country)
 values
-('CN001' ,'John Cena', 'Spain')
-('CN020' ,'Smith Jones', 'Russia')
-('CN01' ,'Albert Walker', 'Germany')
-('CN021' ,'Rosa stines', 'Italy')
+('CN001','John Keena','Spain'),
+('CN020','Smith Jones','Russia'),
+('CN011','Albert Walker','Germany'),
+('CN021','Rosa Stines','Italy');
 
---get all the values store for Cust_Dtails table
-select * from Cust_Details
+-- Get all the values store in the 'Cust_Details' table
+select * from Cust_Details;
 
+-- 3. Create a non-clustered index on the country field
+create index ixCountry on Cust_Details(Country);
 
---3. create a non-clustered index on the country field
-create index ixCountry on Cust_Details(Country)
+-- Create a clustered index on the ProductID field of the product_details table
+Create clustered index ix_ProductID on dbo.product_details(ProductID);
 
--- create a clustered index on the ProductID field of the Product_details
-Create clustered index ixProductID on dbo.Product_details(ProductID)
+-- Create a non-clustered index on the city field in the 'Customer_Details'
+create nonclustered index ixCity on Customer_Details(City);
 
--- create a non-clustered index on the city field in the 'Customer_Details'
-create index ixCity on Cust_Details(City)
+-- Add a primary key constraint to the 'CricketTeam' table
+Alter table dbo.Cricketteam
+add constraint PK_TeamID primary key clustered(TeamID);
 
---add a primary key constraint to the CricketTeam table
-alter table dbo.Cricketteam
-add constraint PK_TeamID primary key clustered(TeamID)
-
---create a primary xml ndex on the CricketTeam table on the teaminfo column/field
-create primary xmlindex PXML_Teaminfo
+-- Create a primary xml index on the 'CricketTeam' table on the teaminfo column/field.
+Create primary xml index PXML_Teaminfo
 on dbo.cricketTeam(teaminfo)
 
---create a secondary index for value() => optimises value() method which is useful when extracting scalar values
-create XML index SXML_TeamInfo_Value
+-- Create a secondary index for value => optimises value() method which is useful when extracting scalar values
+Create XML Index SXML_TeamInfo_Value
 on dbo.CricketTeam(Teaminfo)
-using XML index PXML_Teaminfo
-for value 
+Using XMl Index PXML_Teaminfo
+for Value;
 
---create a secondary index for path() => optimises exists() method and path based lookup
-create XML index SXML_TeamInfo_Path
+-- Create a secondary index for path => optimises exists() method and path based lookups
+Create XML Index SXML_TeamInfo_Path
 on dbo.CricketTeam(Teaminfo)
-using XML index PXML_Teaminfo
-for Path 
+Using XMl Index PXML_Teaminfo
+for Path;
 
---create a secondary index for Property() best used with typed xml columns (the teaminfor column is using the CrcketSchemaCollection Xsd)
-create XML index SXML_TeamInfo_Property
+-- Create a secondary index for Property => best used with typed xml columns (the teaminfor column is using the CricketSchemaCollection xsd)
+Create XML Index SXML_TeamInfo_Property
 on dbo.CricketTeam(Teaminfo)
-using XML index PXML_Teaminfo
-for Property 
+Using XMl Index PXML_Teaminfo
+for Property;
 
---1. ceate a non-clustered index on the productname field in the Product_details table. crea 
-create index ixproductname on dbo.Product_Details(ProductName);
+-- TODO: 1. Create a non-clustered index on the productname field in the 'Product_Details' table.
+create index ixProdName on dbo.product_details(ProductName);
 
---modify/alter the name of the ixProdName non-clustered index to 'IX_ProductName'
-exex sp_rename N'dbo.product_details.ixProdname', N'IX_ProductName', N'Index';
+-- Modify/alter the name of the ixProdName non-clustered index to 'IX_ProductName'
+exec sp_rename N'dbo.product_details.ixProdName', N'IX_ProductName', N'Index';
 
---modify/alter the IX_ProductName' non-clustered index to disable it
-alter index IX_ProductNme on dbo.Producct_details Disable;
+-- Modify/alter the 'IX_ProductName' non-clustered index to disable it
+alter index IX_ProductName on dbo.product_details Disable;
 
---modify/alter the IX_ProductName' non-clustered index to disable it
-alter index IX_ProductNme on dbo.Producct_details rebuild;
+-- Modify/alter the 'IX_ProductName' non-clustered index to enable it
+alter index IX_ProductName on dbo.product_details rebuild;
 
---remove/delete the iX_productName non-clustered index if it exists 
-drop index if exists IX_ProductName on dbo.Product _details;
+-- Remove/delete the 'IX_ProductName'non-clustered index if it exists
+drop index if exists IX_ProductName on dbo.Product_details;
 
---create a table with computed values then index the computed column field 
+-- Create a table with computed values then index the computed column field
 if OBJECT_ID('tblCalcArea') is null
-	create table tblCalcArea 
+	create table tblCalcArea
 	(
-		length decimal(10,2)
-		breadth decimal(10,2)
-		area as length*breadth-- => computed column given by length multiplied by width
-
+		Length Decimal(10,2),
+		Breadth Decimal(10,2),
+		Area as length * breadth -- => computed column given by multiplying the length * breadth to get the shape's area
+	)
 else
-	 print the table tblCalcArea table already existsand will not be recreated
+	print('The ''tblCalcArea'' table already exists and will not be recreated!')
 
---add records into the table tblCalcArea table
-insert into tblCalcArea(length, breadth)
-values 
-(34,10)
-(20,200)
-(33.4,12)
-(12,7)
+-- Add records in to the 'tblCalcArea' table
+insert into tblCalcArea(Length,Breadth)
+values
+(34, 10),
+(20, 20),
+(33.4, 12),
+(12, 7);
 
---check whether the records were inseerted successully
-select* from tblCalcArea
+-- Check whether the records were inserted successfully
+Select * from tblCalcArea;
 
---create an index on the area computed column
-create index ixArea on dbo.tblCalcArea(Areaa);
+-- Create an index on the area computed column
+create index ixArea on tblCalcArea(Area);
 
---the above index will be used in a querry to get shapes with an area less than 400
-select*
+-- The above index will be used in a query to get shapes with an area less than 400
+Select *
 from tblCalcArea
-where Area<400
+where Area < 400;
 
---create a unique index on the 'Emp_Cellular' phone table for the personid column
-create unique index ixPersonID on dbo.emp_cellularphone (PersonID);
+-- Create a unique index on the 'Emp_Cellular' phone table for the personid column
+create unique index ixPersonID on dbo.emp_cellularphone(PersonID);
 
---create a filtered index for products sold for 4000or more on the product_details table 
-create index ixExpensiveProduct on dbo.Product_Details(rate)
+-- Create a filtered index for products sold for 4000 or more on the product_details table
+Create index ixExpensiveProduct on dbo.product_details(rate)
 where rate >= 4000;
 
---use the above index to get products costing 4000 or more
-select ProductID, ProductName [Product Name], Rate, coalasce(Description) [Descriptoon] --use coalesce to remove nulls from the resulttest
-from Product_details where Rate >= 4000;
+-- Use the above index to get products costing 4000 or more
+Select ProductID, ProductName [Product Name], Rate, coalesce(Description,'') [Description] -- use coalesce function to remove nulls from the resultset
+from Product_Details
+where Rate >= 4000;
 
+-- ---------------------------------------------------------------------------
+-- Demonstrate working with cursors
+-- ---------------------------------------------------------------------------
 --1. Create an Employee's Table
 Create Table Employee
 (
@@ -129,7 +133,7 @@ Create Table Employee
 	Salary int not null,
 	Address nvarchar(200) not null
 );
- 
+
 --2. Insert employee records
 Insert into dbo.Employee
 values
@@ -138,10 +142,10 @@ values
 (3,'Alan', 22000, 'New York'),
 (4,'Matthew', 22000, 'Las Vegas'),
 (5,'Joseph', 28000, 'Chicago');
- 
+
 --3. Confirm entry of records into the Employee's Table
 Select * from Employee;
- 
+
 --4. Declare a cursor on the Employee's Table
 set nocount on
 declare @id int, @name nvarchar(100), @salary int
@@ -172,4 +176,3 @@ Close curEmp
 --with the cursor
 deallocate curEmp
 set nocount off
-
